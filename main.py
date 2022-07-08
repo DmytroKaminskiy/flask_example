56e5c64cf4c461647bda13aa2b55068c9786a93b
DmytroKaminskiy <dmytro.kaminskyi92@gmail.com>
2022-07-08 19:34:56 +0300

    test2 branch commit2

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def email_create():
     email = request.args['email']
     name = request.args['name']
+    print('Another one')
 
     try:
         conn = sqlite3.connect('users.db')
